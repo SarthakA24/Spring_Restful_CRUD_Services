04a3e875250aa0c68466c0317cf1282bc5b5d363
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 14:27:50 +0530

    added getDataById method

--- src/main/java/io/github/sarthaka24/restfulservice/service/IStudentService.java
+++ src/main/java/io/github/sarthaka24/restfulservice/service/IStudentService.java
@@ -1,11 +1,14 @@
 package io.github.sarthaka24.restfulservice.service;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IStudentService<T> {
     T saveData(T t);
 
     void deleteData(int id);
+
+    Optional<T> getDataById(int id);
 
     List<T> getData();
 }
