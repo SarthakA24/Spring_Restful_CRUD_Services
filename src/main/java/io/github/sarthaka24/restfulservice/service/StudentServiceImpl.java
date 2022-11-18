9c2fc2858a120c6428c83993124db70554710d89
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 14:28:05 +0530

    implemented the getDataById method

--- src/main/java/io/github/sarthaka24/restfulservice/service/StudentServiceImpl.java
+++ src/main/java/io/github/sarthaka24/restfulservice/service/StudentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements IStudentService<Student> {
@@ -24,6 +25,11 @@ public class StudentServiceImpl implements IStudentService<Student> {
     @Override
     public void deleteData(int id) {
         studentRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Student> getDataById(int id) {
+        return studentRepository.findById(id);
     }
 
     @Override
