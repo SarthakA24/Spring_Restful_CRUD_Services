99fdb8cdcd02768b7b994f8df8eb5724e26823fe
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 10:56:33 +0530

    implemented StudentService interface and override the methods

--- src/main/java/io/github/sarthaka24/restfulservice/service/StudentServiceImpl.java
+++ src/main/java/io/github/sarthaka24/restfulservice/service/StudentServiceImpl.java
@@ -1,4 +1,27 @@
 package io.github.sarthaka24.restfulservice.service;
 
-public class StudentServiceImpl {
+import io.github.sarthaka24.restfulservice.domain.Student;
+
+import java.util.List;
+
+public class StudentServiceImpl implements IStudentService<Student> {
+    @Override
+    public boolean saveData(Student student) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteData(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean updateData(Student student) {
+        return false;
+    }
+
+    @Override
+    public List<Student> getData() {
+        return null;
+    }
 }
