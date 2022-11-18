ef94c4f9e725c6ff3b1570a7297fd4c78fc0856e
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 10:59:51 +0530

    added autowired object for StudentRepository

--- src/main/java/io/github/sarthaka24/restfulservice/service/StudentServiceImpl.java
+++ src/main/java/io/github/sarthaka24/restfulservice/service/StudentServiceImpl.java
@@ -1,12 +1,21 @@
 package io.github.sarthaka24.restfulservice.service;
 
 import io.github.sarthaka24.restfulservice.domain.Student;
+import io.github.sarthaka24.restfulservice.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class StudentServiceImpl implements IStudentService<Student> {
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @Override
     public boolean saveData(Student student) {
         return false;
