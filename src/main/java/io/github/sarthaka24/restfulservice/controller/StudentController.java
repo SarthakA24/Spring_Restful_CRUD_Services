6f9147e2d63a769fb9d410c6447fdfdfe11689c1
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 11:51:32 +0530

    added @RequestMapping annotation

--- src/main/java/io/github/sarthaka24/restfulservice/controller/StudentController.java
+++ src/main/java/io/github/sarthaka24/restfulservice/controller/StudentController.java
@@ -1,7 +1,9 @@
 package io.github.sarthaka24.restfulservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class StudentController {
 }
