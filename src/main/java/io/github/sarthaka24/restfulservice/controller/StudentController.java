4e6b3ba66fd0221c539431a60e1753231c9fd56c
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 11:52:38 +0530

    added object of IStudentService

--- src/main/java/io/github/sarthaka24/restfulservice/controller/StudentController.java
+++ src/main/java/io/github/sarthaka24/restfulservice/controller/StudentController.java
@@ -1,9 +1,18 @@
 package io.github.sarthaka24.restfulservice.controller;
 
+import io.github.sarthaka24.restfulservice.domain.Student;
+import io.github.sarthaka24.restfulservice.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class StudentController {
+    private IStudentService<Student> studentService;
+
+    @Autowired
+    public StudentController(IStudentService<Student> studentService) {
+        this.studentService = studentService;
+    }
 }
