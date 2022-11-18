212820b0ed69bcf21bf036175c2e4ac4277091f6
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 12:56:15 +0530

    added GetMapping and DeleteMapping methods

--- src/main/java/io/github/sarthaka24/restfulservice/controller/StudentController.java
+++ src/main/java/io/github/sarthaka24/restfulservice/controller/StudentController.java
@@ -5,10 +5,7 @@ import io.github.sarthaka24.restfulservice.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -23,5 +20,16 @@ public class StudentController {
     @PostMapping("/add-student")
     public ResponseEntity<?> saveStudent(@RequestBody Student student) {
         return new ResponseEntity<>(studentService.saveData(student), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<?> fetchAllStudent() {
+        return new ResponseEntity<>(studentService.getData(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete-student/{id}")
+    public ResponseEntity<?> deleteStudent(@PathVariable int id) {
+        studentService.deleteData(id);
+        return new ResponseEntity<>("Student Deleted Successfully!", HttpStatus.OK);
     }
 }
