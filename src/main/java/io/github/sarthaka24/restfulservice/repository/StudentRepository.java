78d89d2cac09d992db5a234efe48c222274d4d83
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 10:32:36 +0530

    made StudentRepository extends CrudRepository

--- src/main/java/io/github/sarthaka24/restfulservice/repository/StudentRepository.java
+++ src/main/java/io/github/sarthaka24/restfulservice/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package io.github.sarthaka24.restfulservice.repository;
 
+import io.github.sarthaka24.restfulservice.domain.Student;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentRepository {
+public interface StudentRepository extends CrudRepository<Student, Integer> {
 }
