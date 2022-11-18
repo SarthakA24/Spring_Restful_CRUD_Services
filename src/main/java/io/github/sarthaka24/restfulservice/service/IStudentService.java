c9db280ef67fd3c9aa24b3f3cb68b2d9068a8229
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 11:08:33 +0530

    updated return types of IStudentService interface

--- src/main/java/io/github/sarthaka24/restfulservice/service/IStudentService.java
+++ src/main/java/io/github/sarthaka24/restfulservice/service/IStudentService.java
@@ -3,11 +3,9 @@ package io.github.sarthaka24.restfulservice.service;
 import java.util.List;
 
 public interface IStudentService<T> {
-    boolean saveData(T t);
+    T saveData(T t);
 
-    boolean deleteData(int id);
-
-    boolean updateData(T t);
+    void deleteData(int id);
 
     List<T> getData();
 }
