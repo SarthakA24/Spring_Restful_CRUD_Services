af5cd14240662926288a698677fc4efc03c362e7
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 10:23:54 +0530

    added fields, constructor, getter and setter and toString methods for Student class

--- src/main/java/io/github/sarthaka24/restfulservice/domain/Student.java
+++ src/main/java/io/github/sarthaka24/restfulservice/domain/Student.java
@@ -1,4 +1,60 @@
 package io.github.sarthaka24.restfulservice.domain;
 
 public class Student {
+    private int rollNumber;
+    private String name;
+    private String email;
+    private long contactNumber;
+
+    public Student() {
+    }
+
+    public Student(int rollNumber, String name, String email, long contactNumber) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.email = email;
+        this.contactNumber = contactNumber;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNumber=" + rollNumber +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", contactNumber=" + contactNumber +
+                '}';
+    }
 }
