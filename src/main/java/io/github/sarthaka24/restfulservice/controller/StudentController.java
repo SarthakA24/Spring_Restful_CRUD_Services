cbbd3eb11d9d167262be0c4c2dfe8a0482184696
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-18 12:04:25 +0530

    added PostMapping annotation to save data

--- src/main/java/io/github/sarthaka24/restfulservice/controller/StudentController.java
+++ src/main/java/io/github/sarthaka24/restfulservice/controller/StudentController.java
@@ -3,6 +3,10 @@ package io.github.sarthaka24.restfulservice.controller;
 import io.github.sarthaka24.restfulservice.domain.Student;
 import io.github.sarthaka24.restfulservice.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,10 @@ public class StudentController {
     @Autowired
     public StudentController(IStudentService<Student> studentService) {
         this.studentService = studentService;
+    }
+
+    @PostMapping("/add-student")
+    public ResponseEntity<?> saveStudent(@RequestBody Student student) {
+        return new ResponseEntity<>(studentService.saveData(student), HttpStatus.CREATED);
     }
 }
