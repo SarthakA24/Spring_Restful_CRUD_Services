74160a59b0fffe7c5001af640fbc637499d3f58f
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-22 14:17:05 +0530

    reformatted the document

--- src/main/java/io/github/sarthaka24/restfulservice/repository/StudentRepository.java
+++ src/main/java/io/github/sarthaka24/restfulservice/repository/StudentRepository.java
@@ -1,9 +1,9 @@
 package io.github.sarthaka24.restfulservice.repository;
 
 import io.github.sarthaka24.restfulservice.domain.Student;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends CrudRepository<Student, Integer> {
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
