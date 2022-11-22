9737b98887a780394ebb7fd8eac8e59f9ee7ef8e
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-22 14:29:59 +0530

    added method to find student by name in StudentRepository

--- src/main/java/io/github/sarthaka24/restfulservice/repository/StudentRepository.java
+++ src/main/java/io/github/sarthaka24/restfulservice/repository/StudentRepository.java
@@ -4,6 +4,9 @@ import io.github.sarthaka24.restfulservice.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    List<Student> findByName(String name);
 }
